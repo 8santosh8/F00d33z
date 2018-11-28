d3c27d81e2352e12bcaab2b688aa21f818d686c6
Hemanth <33123731+Hemanth759@users.noreply.github.com>
2018-11-28 13:23:24 +0530

    Changed the static folder to main directory

--- Foodezz/Foodezz/settings.py
+++ Foodezz/Foodezz/settings.py
@@ -136,7 +136,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'templates'),
+    os.path.join(BASE_DIR,'static'),
     ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'Media')
