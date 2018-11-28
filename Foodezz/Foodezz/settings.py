e6b5d30c076a1cbedccd8fe84cf1ae41f89a1a88
Hemanth <33123731+Hemanth759@users.noreply.github.com>
2018-11-28 13:20:24 +0530

    Changed the database to mysql

--- Foodezz/Foodezz/settings.py
+++ Foodezz/Foodezz/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'social_django',
     'materialize',
     'widget_tweaks',
+
 ]
 
 MIDDLEWARE = [
@@ -80,9 +81,17 @@ WSGI_APPLICATION = 'Foodezz.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'mysql.connector.django',
+        'NAME': 'Foodezz',
+        'USER': os.environ.get('MySQLUser'),
+        'PASSWORD': os.environ.get('MySQLPass'),
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
