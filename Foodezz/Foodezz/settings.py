89760c52cd8bc663840931e9d0f5be9fb2ed4d8a
Kartik Wadhwa <kartikwadhwa12@gmail.com>
2018-12-06 21:51:22 +0530

    changes made in templates of the home page

--- Foodezz/Foodezz/settings.py
+++ Foodezz/Foodezz/settings.py
@@ -83,18 +83,18 @@ WSGI_APPLICATION = 'Foodezz.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    # }
-    'default': {
-        'ENGINE': 'mysql.connector.django',
-        'NAME': 'Foodezz',
-        'USER': os.environ.get('MySQLUser'),
-        'PASSWORD': os.environ.get('MySQLPass'),
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
-    }
+     'default': {
+         'ENGINE': 'django.db.backends.sqlite3',
+         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+     }
+    #'default': {
+     #   'ENGINE': 'mysql.connector.django',
+      #  'NAME': 'Foodezz',
+       # 'USER': os.environ.get('MySQLUser'),
+       # 'PASSWORD': os.environ.get('MySQLPass'),
+       # 'HOST': '127.0.0.1',
+       # 'PORT': '3306',
+    
 }
 
 
