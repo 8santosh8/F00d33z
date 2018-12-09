0bb4ca81ec1a76a35de4198ff0e4b285771cc6b3
aman566 <amangupta566@gmail.com>
2018-12-09 12:45:35 +0530

    Removed Unused data

--- F00d3zz/payment/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns=[
-    path('',views.restaurantselection,name='restaurantselection'),
-    path('foodselection',views.foodselection,name='foodselection'),
-]
