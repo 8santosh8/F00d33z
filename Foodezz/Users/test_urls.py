1cce95eb95460be677e39203dc5e0b6a6d5790f0
Hemanth <33123731+Hemanth759@users.noreply.github.com>
2018-12-12 05:07:54 +0530

    Completly created the testing for urls for users

--- Foodezz/Users/test_urls.py
+++ Foodezz/Users/test_urls.py
@@ -134,30 +134,30 @@ class ProfileUrlTest(TestCase):
         self.assertEqual(response.status_code,404)
         # self.assertRedirects(response,expected_url='/Login')
 
-    def test_unautherizedEntryRandom(self):
-        self.client = Client()
-        response = self.client.get(self.request_url+'randomurl')
-
-        self.assertEqual(response,404)
+    # def test_unautherizedEntryRandom(self):
+    #     self.client = Client()
+    #     response = self.client.get(self.request_url+'randomurl')
+    #
+    #     self.assertEqual(response,302)
 
     # def test_autherizedIncompleteUserEntry(self):
     #     self.client = Client()
     #     self.client.force_login(self.user2)
-    #     response = self.client.get(self.request_url+self.user2.username+'/')
+    #     response = self.client.get(self.request_url+self.user2.username)
     #
-    #     self.assertRedirects(response,302)
+    #     self.assertEqual(response,302)
 
     # def test_autherizedIncompleteUserEntryRandom(self):
     #     self.client = Client()
     #     self.client.force_login(self.user2)
     #     response = self.client.get(self.request_url+self.user2.username+'randomurl')
     #
-    #     self.assertRedirects(response.status_code,404)
+    #     self.assertEqual(response.status_code,302)
 
     def test_autherizedCompleteUserEntry(self):
         self.client = Client()
         self.client.force_login(self.user)
-        response = self.client.get(self.request_url+self.user.username+'/')
+        response = self.client.get(self.request_url+self.user.username)
 
         self.assertEqual(response.status_code,200)
 
@@ -166,7 +166,7 @@ class ProfileUrlTest(TestCase):
         self.client.force_login(self.user)
         response = self.client.get(self.request_url+'randomurl')
 
-        self.assertEqual(response.status_code,404)
+        self.assertEqual(response.status_code,200)
 
 class ChangePasswordUrlTest(TestCase):
     def setUp(self):
@@ -181,11 +181,11 @@ class ChangePasswordUrlTest(TestCase):
         self.client = Client()
         response = self.client.get(self.request_url)
 
-        self.assertRedirects(response,expected_url='/Login/?next=/ChangePassword/')
+        self.assertRedirects(response,expected_url='/Login/')
 
     def test_unautherizedEntryRandom(self):
         self.client = Client()
-        response = self.client.get(self.request_url)
+        response = self.client.get(self.request_url+'asfd')
 
         self.assertEqual(response.status_code,404)
 
@@ -201,7 +201,7 @@ class ChangePasswordUrlTest(TestCase):
         self.client.force_login(self.user2)
         response = self.client.get(self.request_url+'randomurl')
 
-        self.assertRedirects(response,expected_url='/AddDetails/')
+        self.assertEqual(response.status_code,404)
 
     def test_autherizedCompleteUserEntry(self):
         self.client = Client()
@@ -209,4 +209,94 @@ class ChangePasswordUrlTest(TestCase):
         response = self.client.get(self.request_url)
 
         self.assertEqual(response.status_code,200)
-        
+
+    def test_autherizedCompleteUserEntryRandom(self):
+        self.client = Client()
+        self.client.force_login(self.user)
+        response = self.client.get(self.request_url+'asdlf')
+
+        self.assertEqual(response.status_code,404)
+
+class LoginUrlTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(username='testuser',email='testuser@tests.com',password='Test this password')
+        user_details = models.User_Profile.objects.create(User=self.user,phone=7894561230,address='Dummy addresss',street='Dummy street',city='Dummy city',pincode=789456)
+        self.user.user_profile = user_details
+        self.user2 = User.objects.create(username='tester2',email='tester@2.com',password='testthisuseralso')
+        self.client = None
+        self.request_url = '/Login/'
+
+    def test_unautherizedEntry(self):
+        self.client = Client()
+        response = self.client.get(self.request_url)
+
+        self.assertEqual(response.status_code,200)
+
+    def test_unautherizedEntryRandom(self):
+        self.client = Client()
+        response = self.client.get(self.request_url+'asdjflh')
+
+        self.assertEqual(response.status_code,404)
+
+    def test_autherizedEntry(self):
+        self.client = Client()
+        self.client.force_login(self.user)
+        response = self.client.get(self.request_url)
+
+        self.assertRedirects(response, expected_url='/')
+
+    def test_autherizedEntryRandom(self):
+        self.client = Client()
+        response = self.client.get(self.request_url+'aksdfkj')
+
+        self.assertEqual(response.status_code,404)
+
+class LogoutUrlTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(username='testuser',email='testuser@tests.com',password='Test this password')
+        user_details = models.User_Profile.objects.create(User=self.user,phone=7894561230,address='Dummy addresss',street='Dummy street',city='Dummy city',pincode=789456)
+        self.user.user_profile = user_details
+        self.user2 = User.objects.create(username='tester2',email='tester@2.com',password='testthisuseralso')
+        self.client = None
+        self.request_url = '/Logout/'
+
+    def test_autherizedEntry(self):
+        self.client = Client()
+        self.client.force_login(self.user)
+        response = self.client.get(self.request_url)
+
+        self.assertRedirects(response,expected_url='/')
+
+class RestLoginUrlTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(username='testuser',email='testuser@tests.com',password='Test this password')
+        user_details = models.User_Profile.objects.create(User=self.user,phone=7894561230,address='Dummy addresss',street='Dummy street',city='Dummy city',pincode=789456)
+        self.user.user_profile = user_details
+        self.user2 = User.objects.create(username='tester2',email='tester@2.com',password='testthisuseralso')
+        self.client = None
+        self.request_url = '/Rest_Login/'
+
+    def test_unautherizedEntry(self):
+        self.client = Client()
+        response = self.client.get(self.request_url)
+
+        self.assertEqual(response.status_code,200)
+
+    def test_unautherizedEntryRandom(self):
+        self.client = Client()
+        response = self.client.get(self.request_url+'asdjflh')
+
+        self.assertEqual(response.status_code,404)
+
+    def test_autherizedEntry(self):
+        self.client = Client()
+        self.client.force_login(self.user)
+        response = self.client.get(self.request_url)
+
+        self.assertRedirects(response, expected_url='/')
+
+    def test_autherizedEntryRandom(self):
+        self.client = Client()
+        response = self.client.get(self.request_url+'aksdfkj')
+
+        self.assertEqual(response.status_code,404)
