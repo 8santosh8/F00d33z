2c862a7580d6b09d0e707e356fc2ecc003a3f9b1
Hemanth <33123731+Hemanth759@users.noreply.github.com>
2018-12-12 18:07:19 +0530

    Almost integrated

---
+++ Foodezz/static/js/hotels.js
@@ -0,0 +1,46 @@
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+
+
+
+(function($) {
+
+  // Menu filter
+  $("#menu-flters li a").click(function() {
+    $("#menu-flters li a").removeClass('active');
+    $(this).addClass('active');
+
+    var selectedFilter = $(this).data("filter");
+
+
+    $(".menu-restaurant").fadeOut();
+
+    setTimeout(function() {
+      $(selectedFilter).slideDown();
+    }, 300);
+  });
+
+  // scrolling to all links in navbar and footer link
+  $(".logout").on('click', function(event) {
+    var hash = this.hash;
+    if (hash) {
+      event.preventDefault();
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 300, function() {
+        window.location.hash = hash;
+      });
+    }
+
+  });
+
+  $(".sidenav a").on('click', function() {
+		closeNav();
+	});
+
+})(jQuery);
