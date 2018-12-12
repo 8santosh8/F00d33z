7a10c20bc9c34b261287b9808cb33054b8db4fdc
8santosh8 <santosh.p17@iiits.in>
2018-12-12 18:32:30 +0530

    Fixed some bugs

--- Foodezz/payments/urls.py
+++ Foodezz/payments/urls.py
@@ -6,7 +6,7 @@ app_name = "payments"
 
 urlpatterns=[
     path('search',views.search,name='search'),
-    path('pay',views.pay,name='pay'),
+    path('pay/<rest>',views.pay,name='pay'),
     path('restupdate',views.restupdate,name='restupdate'),
     path('newitem',views.newitem,name='newitem'),
     path('mycart',views.mycart,name='mycart'),
@@ -17,4 +17,6 @@ urlpatterns=[
     path('showrestaurant/<restaurant>', views.showrestaurant, name='showrestaurant'),
     path('makeorderconfirm', views.makeorderconfirm, name='makeorderconfirm'),
     path('makeprev', views.makeprev, name='makeprev'),
+    path('feedback', views.feedback, name='feedback'),
+    path('feedbackthanks', views.feedbackthanks, name='feedbackthanks')
 ]
